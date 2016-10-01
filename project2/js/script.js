9c4873a2dacbfbe88f996fb118661836370826bd
allyjlevine <allyjlevine@gmail.com>
2016-10-01 14:34:41 -0400

    take it from the top

---
+++ project2/js/script.js
@@ -0,0 +1,15 @@
+
+		jQuery(document).ready(function($) {
+      console.log( "ready!" );
+      $('.my-slider').unslider({
+
+				arrows: {
+					//  Unslider default behaviour
+					prev: '<a class="unslider-arrow prev"><i class="fa fa-chevron-left" aria-hidden="true"></i></a>',
+					next: '<a class="unslider-arrow next"><i class="fa fa-chevron-right" aria-hidden="true"></i></a>',
+				}
+
+			});
+
+
+		});
