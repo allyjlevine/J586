3908e63ed3a8d7cf8f11c4d5e35ebb45f7617234
allyjlevine <allyjlevine@gmail.com>
2016-10-15 16:51:08 -0400

    change colors, fix arrow padding

--- project2/chart/chart.js
+++ project2/chart/chart.js
@@ -6,56 +6,10 @@ var horizontalBarChartData = {
     labels: ["Pay Pal", "NCAA", "ACC", "NBA"],
     datasets: [{
         label: 'Lost $',
-        backgroundColor: '#2130E9',
+        backgroundColor: '#5259AB',
         data: [24 , 49 , 33 , 100 ]
     }]
 };
 
 window.onload = function() {
-    // var ctx = document.getElementById("canvas").getContext("2d");
-    // window.myHorizontalBar = new Chart(ctx, {
-    //     type: 'horizontalBar',
-    //     data: horizontalBarChartData,
-    //     options: {
-    //         // Elements options apply to all of the options unless overridden in a dataset
-    //         // In this case, we are setting the border of each horizontal bar to be 2px wide and green
-    //         elements: {
-    //             rectangle: {
-    //                 borderWidth: 0,
-    //                 borderColor: 'rgb(40, 255, 0)',
-    //                 borderSkipped: 'left'
-    //
-    //             }
-    //         },
-    //         responsive: true,
-    //         legend: {
-    //             position: 'right',
-    //             display: false,
-    //         },
-    //         title: {
-    //             display: true,
-    //             text: 'Financial Losses from HB2',
-    //             fontFamily: 'Raleway',
-    //             fontSize: 40,
-    //             fontStyle: 900,
-    //             fontColor: 'black',
-    //             padding: 20,
-    //         },
-    //
-    //         scales: {
-    //           xAxes: [{
-    //             scaleLabel: {
-    //               labelString: 'Millions of Dollars Lost',
-    //               display: true,
-    //             },
-    //             ticks: {
-    //               userCallback: function(tick) {
-    //                 return "$" + tick.toString();
-    //               }
-    //             }
-    //
-    //           }]
-    //         }
-    //     }
-    // });
 };
